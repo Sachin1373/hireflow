f52099ea687961e3e53b848e57f74e39ed813e6a
Sachin1373 <sachinvrathod1373@gmail.com>
2026-05-04 23:48:24 +0530

    db pool updated node

--- apps/api/src/db/client.ts
+++ apps/api/src/db/client.ts
@@ -3,10 +3,7 @@ import dotenv from 'dotenv'
 import path from 'path'
 
 
-dotenv.config({
-  path: path.resolve(__dirname, '../../../../.env')
-})
-
+dotenv.config()
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
