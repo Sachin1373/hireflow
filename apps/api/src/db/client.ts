b940cea3f2a3ec1e5e97baf58a34b33bc1f3d491
Sachin1373 <sachinvrathod1373@gmail.com>
2026-05-04 23:51:04 +0530

    db pool updated node

--- apps/api/src/db/client.ts
+++ apps/api/src/db/client.ts
@@ -5,6 +5,8 @@ import path from 'path'
 
 dotenv.config()
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
